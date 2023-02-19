6d63e649a6a864b290a0d3397eb6405f3317ba75
Alberto Madrazo <betomadrazo@gmail.com>
2023-02-18 19:56:14 -0600

    Up to page 121.

--- todo/todo_crud.ex
+++ todo/todo_crud.ex
@@ -5,11 +5,46 @@ defmodule TodoList do
 
   def add_entry(todo_list, entry) do
     # Al mapa que está en entry, agrégale el campo id, que valdrá lo del 3er argumento
+    # %{entry | id: auto_id} works only if the id field is already present in the map.
     entry = Map.put(entry, :id, todo_list.auto_id)
 
     # A Map.put/3 le puedes poner en el segundo argumento un atom o una constante(número), o una variable
     new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
 
     %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
+
+    # To the external caller, the entire operation will be atomic. Either will happen or,
+    # in case of an error, nothing at all.This is the consequence of inmutability.
+    # The effect of adding an entry is visible
+    # to others only when the add_entry/2 function finishes and its result is taken
+    # into a variable.
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_id, entry} -> entry.date == date end)
+    |> Enum.map(fn {_id, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+      end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, _} ->
+        Map.delete(todo_list.entries, entry_id)
+      end
   end
 end
